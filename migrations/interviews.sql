29e95133521d45a88c668b1cc7589f0021e2151d
KEENTO <handball0321@gmail.com>
2022-10-10 17:03:52 -0700

    Added interviewers_seeds.sql file

--- migrations/interviews.sql
+++ migrations/interviews.sql
@@ -1,1 +1,2 @@
-CREATE TABLE interview(id:SERIAL PRIMARY KEY,student:TEXT,interviewer_id:INTEGER,appointment_id:INTEGER)
+-- Maybe we need day_id here as well
+CREATE TABLE interviews(id:SERIAL PRIMARY KEY,student:TEXT,interviewer_id:INTEGER,appointment_id:INTEGER)
---
+++ seeds/interviewers_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO interviewers (id SERIAL PRIMARY KEY,name TEXT,avatar:TEXT) VALUES (1,'Sylvia Palmer','https://i.imgur.com/LpaY82x.png')
+INSERT INTO interviewers (id SERIAL PRIMARY KEY,name TEXT,avatar:TEXT) VALUES (2,'Tori Malcolm','https://i.imgur.com/Nmx0Qxo.png')
+INSERT INTO interviewers (id SERIAL PRIMARY KEY,name TEXT,avatar:TEXT) VALUES (3,'Mildred Nazir','https://i.imgur.com/T2WwVfS.png')
+INSERT INTO interviewers (id SERIAL PRIMARY KEY,name TEXT,avatar:TEXT) VALUES (4,'Cohana Roy','https://i.imgur.com/FK8V841.jpg')
+INSERT INTO interviewers (id SERIAL PRIMARY KEY,name TEXT,avatar:TEXT) VALUES (5,'Sven Jones','https://i.imgur.com/twYrpay.jpg')
