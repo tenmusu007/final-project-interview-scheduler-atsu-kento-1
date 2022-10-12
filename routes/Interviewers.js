4ad9ba6ba310991c523388d642cc23f7b108ac59
KEENTO <handball0321@gmail.com>
2022-10-13 05:42:51 +0800

    Added some routes related to interviews

--- routes/Interviewers.js
+++ routes/Interviewers.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { Pool } = require("pg");
 
 router.get("/", (req, res) => {
-  res.json({ msg: "Interviewers" });
+  const pool = new Pool({
+    name: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+  pool
+    .query("SELECT * FROM interviewers")
+    .then((result) => result.rows)
+    .then((interviewers) => res.json(interviewers))
+    .catch((err) => console.log(err))
+    .finally(() => pool.end());
 });
 
 module.exports = router;
