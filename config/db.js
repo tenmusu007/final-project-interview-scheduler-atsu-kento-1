f21d7c31b4af1d0896086ce8ef89ad35bbeb653c
Atsuya Tanaka <tenmusu007@gmail.com>
2022-10-13 11:23:46 -0700

    add day

--- config/db.js
+++ config/db.js
@@ -1,14 +1,33 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 const fs = require("fs");
+// console.log(process.env.ATSUYA);
+const pool = new Pool({
+	user: process.env.NAME,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT,
+});
+// console.log("pool", pool);
 
-const queryDb = async () => {
-  const pool = new Pool({
-		user: process.env.USERNAME,
-		host: process.env.HOST,
-		database: process.env.DATABASE,
-		password: process.env.PASSWORD,
-		port: process.env.PORT,
-	});
-};
 
-module.exports = { queryDb };
+
+// const pool = new Pool({
+// 	user: "postgres",
+// 	host: "localhost",
+// 	database: "nodefinal",
+// 	password: "atsu0301",
+// 	port: 5432,
+// });
+// const queryDb = async () => {
+// 	const pool = new Pool({
+// 		user: process.env.USERNAME,
+// 		host: process.env.HOST,
+// 		database: process.env.DATABASE,
+// 		password: process.env.PASSWORD,
+// 		port: process.env.PORT,
+// 	});
+// };
+
+module.exports = { pool };
