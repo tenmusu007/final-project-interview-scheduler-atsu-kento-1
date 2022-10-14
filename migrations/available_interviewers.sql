02afd08717c60b5af018b2a5f2bd3cf14a0e976a
KEENTO <handball0321@gmail.com>
2022-10-14 07:19:51 +0700

    Added queries directory for testing sql

--- migrations/available_interviewers.sql
+++ migrations/available_interviewers.sql
@@ -1,1 +1,1 @@
-CREATE TABLE available_interviewers(id SERIAL PRIMARY KEY,interviewer_id:INTEGER,day_id:INTEGER)
+CREATE TABLE available_interviewers(id SERIAL PRIMARY KEY,interviewer_id INTEGER,day_id INTEGER)
--- migrations/interviews.sql
+++ migrations/interviews.sql
@@ -1,2 +1,2 @@
 -- Maybe we need day_id here as well
-CREATE TABLE interviews(id:SERIAL PRIMARY KEY,student:TEXT,interviewer_id:INTEGER,appointment_id:INTEGER)
+CREATE TABLE interviews(id SERIAL PRIMARY KEY,student TEXT,interviewer_id INTEGER,appointment_id INTEGER)
