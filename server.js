c97fa4d591536e9257b24b0834b3282dbc05e9e5
KEENTO <handball0321@gmail.com>
2022-10-16 21:27:15 +0700

    Modified socket.io

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cors());
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "DELETE"],
   },
 });
 io.on("connection", (socket) => {
